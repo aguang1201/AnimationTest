4d1dfb054ab5a33c225d4ac01c4ed6eb7d2c8cb1
zhou Mr <uubs@yeah.net>
2016-05-28 21:16:54 +0900

    Initial Commit

---
+++ AnimationTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimationTest
+//
+//  Created by Mrzhou on 16/5/28.
+//  Copyright © 2016年 Mrzhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
